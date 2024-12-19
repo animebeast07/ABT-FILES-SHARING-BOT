1ecdb74061854858df59f0dbd8adc9c9c699fd82
animebeast07 <narutouzumakitamil2005@gmail.com>
2024-12-19 13:48:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,6 +33,9 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Animebeasttamil")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002075562432"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002063724494"))
+FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "-1002249144915"))
+
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
